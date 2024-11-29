44217a0b57f1d6e96b05f59bde1871bc9e2ef2ce
abbie-p <abbie.paulson1@gamil.com>
2024-11-28 17:50:23 -0700

    Added fractional activation

---
+++ assignment_order.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class Assignment_order():
+
+    def __init__(self, num_resid, noes):
+        self.num_resid = num_resid # value would need to be the number of shifts/atoms
+        self.noes = noes
+
+    def activation_loop(self, target, noe, assign_order):
+        s = set()
+        for combo in noe:
+            if combo[0] not in assign_order: # check if values have already been assigned as higher priority
+                s.add(combo[0])
+            if combo[1] not in assign_order:
+                s.add(combo[1])
+
+        if target in s:
+            return 1/(len(s))
+        else:
+            return 0
+
+    def fractional_activation(self):
+        assign_order = []
+        for j in range(self.num_resid): # I don't know why I need this loop here it just makes it work
+            temp2 = []
+            for i in range(self.num_resid):
+                temp =  0
+                for noe in self.noes:
+                    temp += self.activation_loop(i, noe, assign_order) # sum up the probabilities for 'i' (the given shift target)
+                temp2.append(temp)
+
+            assign_order.append(np.argmax(temp2)) # append the max value's index (takes first occurence if equivalent)
+
+        return assign_order
+
+
+    
